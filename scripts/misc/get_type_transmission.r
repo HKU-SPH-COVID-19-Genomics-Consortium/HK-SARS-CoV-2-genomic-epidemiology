15afd42211c1fdab86a7c29004c3454d6eb21e3e
Haogao Gu <koohoko@gmail.com>
2021-06-17 16:52:06 +0800

    update transmission type

--- scripts/misc/get_type_transmission.r
+++ scripts/misc/get_type_transmission.r
@@ -12,7 +12,8 @@ df_rst$case.id <- sapply(df_rst$seq_name, function(x){
 
 df_metadata <- read_excel("../../data/metadata_2021-04-30.xlsx", col_types = "text")
 table(df_metadata$reclassification)
-df_metadata <- df_metadata %>% filter(grepl("ocal", reclassification)) 
+# df_metadata <- df_metadata %>% filter(grepl("ocal", reclassification)) 
+df_metadata <- df_metadata %>% filter(reclassification != "Imported") 
 table(df_metadata$reclassification)
 
 df_metadata$wave <- mclapply(df_metadata$case.id, function(x){
@@ -54,9 +55,9 @@ df_primary_type <- bind_rows(df_primary_type)
 
 sort(table(df_primary_type$type)) 
 ## social
-df_primary_type$type[df_primary_type$type %in% c("friends", "meal", "dating", "MK", "GreenRiver", "BunKeeCongee", "Bldg", "Windsor", "mahjong", "TuenMun", "VictoriaHarbour", "gathering", "hotel", "tutorial", "TST", "table", "Sunfat", "restaurant", "luckydragon", "Delux", "Dating", "boattrip", "boardinghouse", "港九小輪", "royal garden", "China Secret", "singing", "seaview resort", "Gateway", "danceclub", "Bar and Band", "worship", "karaoke", "Otto", "GlowSpa", "土瓜灣喜宴酒樓", "privatetutor", "gym", "DimSumSquare", "church", "wedding", "friend", "sheltered", "Kagesha2", "Kagesha1", "BirthdayParty", "bar", "松山", "travel", "Superstar", "outback TST", "M&S", "hugging", "gym45", "facial", "ChinaBar", "chatting", "beautyparlour")] <- "Social"
+df_primary_type$type[df_primary_type$type %in% c("friends", "meal", "dating", "MK", "GreenRiver", "BunKeeCongee", "Bldg", "Windsor", "mahjong", "TuenMun", "VictoriaHarbour", "gathering", "hotel", "tutorial", "TST", "table", "Sunfat", "restaurant", "luckydragon", "Delux", "Dating", "boattrip", "boardinghouse", "港九小輪", "royal garden", "China Secret", "singing", "seaview resort", "Gateway", "danceclub", "Bar and Band", "worship", "karaoke", "Otto", "GlowSpa", "土瓜灣喜宴酒樓", "privatetutor", "gym", "DimSumSquare", "church", "wedding", "friend", "sheltered", "Kagesha2", "Kagesha1", "BirthdayParty", "bar", "松山", "travel", "Superstar", "outback TST", "M&S", "hugging", "gym45", "facial", "ChinaBar", "chatting", "beautyparlour", "Bus", "Coffee", "Travel")] <- "Social"
 ## Family/Roommate
-df_primary_type$type[df_primary_type$type %in% c("roommate", "fam", "麗晶Block6", "Block 8 Kwai Shing West", "Holly Mansion", "星月居", "estate", "care", "星月樓", "泉章居", "LukChuen", "明麗樓", "building", "仁石樓Unit 09", "Roommate", "neighbour", "MeiFoo", "maid", "麗港城第五座", "貴東樓")] <- "Family/Roommate"
+df_primary_type$type[df_primary_type$type %in% c("roommate", "fam", "麗晶Block6", "Block 8 Kwai Shing West", "Holly Mansion", "星月居", "estate", "care", "星月樓", "泉章居", "LukChuen", "明麗樓", "building", "仁石樓Unit 09", "Roommate", "neighbour", "MeiFoo", "maid", "麗港城第五座", "貴東樓", "Partner")] <- "Family/Roommate"
 ## Work
 df_primary_type$type[df_primary_type$type %in% c("maid quarter", "work", "TMH", "taxi", "school", "Kerry Logistics", "KinWing")] <- "Work"
 ## RCHE/RCHD
@@ -78,6 +79,10 @@ df_seqed_stat <- df_seqed %>% group_by(wave) %>% summarise(n_social = sum(type =
 # summary count for all cases
 df_all <- df_primary_type %>% left_join(df_metadata %>% select(case.id, wave))
 write_csv(df_all, "../../results/Misc/transmission_type_All.csv")
+df_all %>% filter(wave %in% c("Wave_3", "Wave_4")) %>% group_by(type, wave) %>% summarise(n = n())  
+df_all %>% group_by(wave) %>% summarise(n = n())
+
+
 df_all_stat <- df_all %>% group_by(wave) %>% summarise(n_social = sum(type == "Social", na.rm = T), n_non_social = sum(type %in% c("Family/Roommate", "Work", "Rche/Rchd", "Nosocomial", na.rm = T)))
 
 # chisq test
