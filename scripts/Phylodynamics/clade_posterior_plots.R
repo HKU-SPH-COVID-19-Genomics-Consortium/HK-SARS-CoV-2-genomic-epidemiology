ed4b547d36562d497666e937460e86864815f243
Dillon C. Adam <dcadam@hku.hk>
2021-10-20 13:39:16 +0800

    figure 2 revisions

--- scripts/Phylodynamics/clade_posterior_plots.R
+++ scripts/Phylodynamics/clade_posterior_plots.R
@@ -1,12 +1,14 @@
 library(tidyverse)
 library(lubridate)
 library(scales)
+library(HDInterval)
+library(ggsci)
 set.seed(1)
 
 
 ##### Due to the large size of the results file, it is not available via GitHub but can be downloaded from the link below:  https://www.dropbox.com/s/es7lhheemlpgm2d/credible_tree_summary_sample_8000.csv?dl=0
 
-credible_tree_summary <- read_csv("results/credible_tree_summary_sample_8000.csv")
+credible_tree_summary <- read_csv("credible_tree_summary_sample_8000.csv")
 
 credible_tree_summary %>%
   mutate(wave = case_when(first_sample_date <= "2020-02-22" ~ "Wave 1",
@@ -69,6 +71,8 @@ lineage_n6 <- credible_tree_summary %>%
 A <- bind_rows(lineage_n19, lineage_n7, lineage_n92, lineage_n29, lineage_n6) %>%
   ggplot() +
   geom_density(aes(x=tmrca, colour = lineage), adjust = 20, size = 0.8) +
+  geom_vline(xintercept = as_date("2020-01-30")) +
+  geom_vline(xintercept = as_date("2020-03-18")) +
   scale_x_date("Lineage TMRCA",
                date_breaks = "1 month", 
                date_labels = "%b/%y",
@@ -83,6 +87,8 @@ A <- bind_rows(lineage_n19, lineage_n7, lineage_n92, lineage_n29, lineage_n6) %>
         legend.position = c(0.85, 0.85), 
         legend.title = element_blank()) +
   ggsci::scale_color_jco()
+
+
 
 
 #### Number of samples per lineage 
@@ -111,12 +117,13 @@ B <- credible_tree_summary %>%
                expand = c(0.1,0.1)) +
   theme_classic() +
   theme(aspect.ratio = 1,
-        legend.position = 'none') +
-  annotation_logticks()
+        legend.position = c(0.9,0.9)) +
+  annotation_logticks() +
+  labs(size = "No. samples")
 
 
 ##Local Lineage duration vs delay in detection
-
+set.seed(1)
 credible_tree_summary_sample <- credible_tree_summary %>% 
   filter(num_samples > 1,
          num_travel_cases == 0) %>%
@@ -134,7 +141,7 @@ C <- credible_tree_summary_sample %>%
                         first_sample_date >= "2020-09-29" & first_sample_date <= "2021-01-26" ~ "Wave 4",
                         TRUE ~ NA_character_)) %>%
   ggplot() +
-  geom_jitter(aes(x = delay, y = undetected_duration, size = num_samples, fill = wave), 
+  geom_jitter(aes(x = delay, y = undetected_duration, fill = wave), 
               height = 5, 
               width = 5, 
               shape = 21,
@@ -157,10 +164,10 @@ C <- credible_tree_summary_sample %>%
   theme_classic() +
   theme(aspect.ratio = 1, 
         legend.position = c(0.85,0.85)) +
-  labs(size = "Samples per lineage") +
+  labs(size = "Samples per lineage", 
+       fill = "") +
   scale_fill_manual(values = c('#52cc83', '#5283cc', '#b452cc'))
 
-C
 
 detection_duration_correlation <- credible_tree_summary %>% 
   filter(num_samples > 1,
@@ -176,11 +183,14 @@ cor.test(detection_duration_correlation$effective_delay, detection_duration_corr
          exact = F)
 
 ##Plot top row
-top_row <- cowplot::plot_grid(A, B, C, labels = c('A', 'B', 'C'), align = 'hv', ncol = 1)
+top_row <- cowplot::plot_grid(A, B, C, labels = c('a', 'b', 'c'), align = 'hv', ncol = 3)
+
 
 
 ### Detection over time by wave
-D <- credible_tree_summary %>%
+
+set.seed(1)
+credible_tree_summary_sample_2 <- credible_tree_summary %>%
   mutate(local = str_detect(samples, "local"),
          wave = case_when(first_sample_date >= "2020-01-23" & last_sample_date <= "2020-05-12" ~ "Wave 1+2",
                           first_sample_date >= "2020-05-13" & last_sample_date <= "2020-09-29" ~ "Wave 3",
@@ -190,17 +200,24 @@ D <- credible_tree_summary %>%
          effective_delay != 0) %>%
   filter(!is.na(wave)) %>%
   group_by(wave) %>%
-  sample_n(size = 1000, replace = F) %>%
-  ggplot() +
-  geom_jitter(aes(x = as_date(tmrca), y = effective_delay, fill = wave),
-              height = 10, 
-              width = 10,
+  sample_n(size = 1000, replace = F)
+
+
+
+sample <- credible_tree_summary_sample_2 %>% 
+  filter(wave == "Wave 1+2")
+
+set.seed(1)
+sample$tmrca_j <- sample$tmrca+runif(sample$tmrca, min = -10, max = 10)
+sample$delay_j <- sample$effective_delay+runif(sample$effective_delay, min = -10, max = 10)
+
+D1 <- ggplot(sample) + 
+  geom_point(aes(x = as_date(tmrca_j), y = delay_j, fill = wave),
               shape = 21,
               alpha = 0.6) +
-  geom_smooth(method = lm, aes(x = as_date(tmrca), y = effective_delay, fill = wave, colour = wave), 
+  geom_smooth(method = "lm", aes(x = as_date(tmrca_j), y = delay_j, fill = wave, colour = wave), 
               se = T, 
-              alpha = 0.5) +
-  facet_wrap(~wave, scales = 'free_x', ncol = 1) +
+              alpha = 0.5, size = 0.5) +
   scale_y_continuous("Detection lag (days)", 
                      limits = c(0,100),
                      breaks = seq(0,100, by = 10),
@@ -214,8 +231,91 @@ D <- credible_tree_summary %>%
         strip.text = element_blank(),
         legend.position = c(0.85,0.85),
         panel.spacing = unit(1.5, "lines"),
-        aspect.ratio = 1) +
-  scale_color_manual(values = c('#52cc83', '#5283cc', '#b452cc')) +
-  scale_fill_manual(values = c('#52cc83', '#5283cc', '#b452cc')) +
-  labs(fill = "Wave", colour = "Wave")
+        aspect.ratio = 1,
+        axis.title.x = element_blank()) +
+  scale_color_manual(values = c('#52cc83')) +
+  scale_fill_manual(values = c('#52cc83')) +
+  labs(fill = "", colour = "") 
 
+
+
+
+
+  
+  
+  sample <- credible_tree_summary_sample_2 %>% filter(wave == "Wave 3")
+  set.seed(1)
+  sample$tmrca_j <- sample$tmrca+runif(sample$tmrca, min = -10, max = 10)
+  sample$delay_j <- sample$effective_delay+runif(sample$effective_delay, min = -10, max = 10)
+
+
+  
+D2 <- ggplot(sample) + 
+    geom_point(aes(x = as_date(tmrca_j), y = delay_j, fill = wave),
+               shape = 21,
+               alpha = 0.6) +
+    geom_smooth(method = "lm", aes(x = as_date(tmrca_j), y = delay_j, fill = wave, colour = wave), 
+                se = T, 
+                alpha = 0.5, size = 0.5) +
+    scale_y_continuous("Detection lag (days)", 
+                       limits = c(0,100),
+                       breaks = seq(0,100, by = 10),
+                       expand = c(0, 0), 
+                       oob = squish) +
+    scale_x_date("TMRCA",
+                 date_breaks = "11 days", 
+                 date_labels = "%d/%b/%y") +
+    theme_classic() +
+    theme(strip.background = element_blank(),
+          strip.text = element_blank(),
+          legend.position = c(0.85,0.85),
+          panel.spacing = unit(1.5, "lines"),
+          aspect.ratio = 1,
+          axis.title.y = element_blank()) +
+    scale_color_manual(values = c('#5283cc')) +
+    scale_fill_manual(values = c('#5283cc')) +
+    labs(fill = "", colour = "") 
+
+
+
+sample <- credible_tree_summary_sample_2 %>% filter(wave == "Wave 4")
+set.seed(1)
+sample$tmrca_j <- sample$tmrca+runif(sample$tmrca, min = -10, max = 10)
+sample$delay_j <- sample$effective_delay+runif(sample$effective_delay, min = -10, max = 10)
+
+
+
+D3 <- ggplot(sample) + 
+  geom_point(aes(x = as_date(tmrca_j), y = delay_j, fill = wave),
+             shape = 21,
+             alpha = 0.6) +
+  geom_smooth(method = "lm", aes(x = as_date(tmrca_j), y = delay_j, fill = wave, colour = wave), 
+              se = T, 
+              alpha = 0.5, size = 0.5) +
+  scale_y_continuous("Detection lag (days)", 
+                     limits = c(0,100),
+                     breaks = seq(0,100, by = 10),
+                     expand = c(0, 0), 
+                     oob = squish) +
+  scale_x_date("TMRCA",
+               date_breaks = "12 days", 
+               date_labels = "%d/%b/%y") +
+  theme_classic() +
+  theme(strip.background = element_blank(),
+        strip.text = element_blank(),
+        legend.position = c(0.85,0.85),
+        panel.spacing = unit(1.5, "lines"),
+        aspect.ratio = 1, 
+        axis.title.x = element_blank(), 
+        axis.title.y = element_blank()) +
+  scale_color_manual(values = c('#b452cc')) +
+  scale_fill_manual(values = c('#b452cc')) +
+  labs(fill = "", colour = "") 
+
+
+bottom_row <- cowplot::plot_grid(D1, D2, D3, labels = c('d', '', ''), align = 'hv', ncol = 3)
+
+
+cowplot::plot_grid(top_row, bottom_row, ncol = 1)
+
+ggsave(filename = "results/Phylodynamics/Fig_2.pdf",  width = 10, height = 7, units = 'in')
